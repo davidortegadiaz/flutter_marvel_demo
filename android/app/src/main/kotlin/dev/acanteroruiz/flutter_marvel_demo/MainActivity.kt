7797b19a9f1a093a8f8aba30b061a92201232901
Antonio Cantero <acr.cuentas@gmail.com>
2021-02-25 15:40:05 +0100

    Initialize project

---
+++ android/app/src/main/kotlin/dev/acanteroruiz/flutter_marvel_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.acanteroruiz.flutter_marvel_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Marvel Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(child: Text('Press the button to fetch characters!!')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Fetch',
+        child: Icon(Icons.download_outlined),
+      ),
+    );
+  }
+}
