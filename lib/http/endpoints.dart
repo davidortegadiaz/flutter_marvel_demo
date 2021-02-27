a2374e228dc186a247752c98311104718d4d2155
Antonio Cantero <acr.cuentas@gmail.com>
2021-02-27 10:12:58 +0100

    Update endpoints

--- lib/http/endpoints.dart
+++ lib/http/endpoints.dart
@@ -1,6 +1,5 @@
 class Endpoints {
-  static const String baseUrl = 'https://gateway.marvel.com';
-  static const String api = '/v1/public';
+  static const String baseUrl = 'https://gateway.marvel.com/v1/public';
   static const String characters = '/characters';
   static const String comics = '/comics';
 }
