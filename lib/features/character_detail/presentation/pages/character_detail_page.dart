b48aa16439c0f558a510f009c8eecbd497172720
Antonio Cantero <acr.cuentas@gmail.com>
2021-03-01 09:45:08 +0100

    Fetch comics before navigate from List to Detail

--- lib/features/character_detail/presentation/pages/character_detail_page.dart
+++ lib/features/character_detail/presentation/pages/character_detail_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_marvel_demo/features/character_detail/presentation/widgets/detail_appbar.dart';
 import 'package:flutter_marvel_demo/features/character_detail/presentation/widgets/detail_description.dart';
+import 'package:flutter_marvel_demo/features/character_detail/presentation/widgets/details_comic_row.dart';
 import 'package:flutter_marvel_demo/features/character_list/domain/models/character.dart';
 import 'package:get/get.dart';
 
@@ -15,54 +16,7 @@ class CharacterDetailPage extends StatelessWidget {
         slivers: [
           DetailAppBar(character: _character),
           DetailDescription(character: _character),
-          //mocked widget
-          ...[_DetailComicsRow()],
-        ],
-      ),
-    );
-  }
-}
-
-class _DetailComicsRow extends StatelessWidget {
-  const _DetailComicsRow({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 10,
-              horizontal: 12,
-            ),
-            child: Text(
-              'COMICS',
-              style: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.w600,
-                color: Colors.black,
-              ),
-            ),
-          ),
-          Container(
-            height: 100.0,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return Container(
-                  width: 100.0,
-                  child: Card(
-                    child: Text('data'),
-                  ),
-                );
-              },
-            ),
-          ),
+          ...[DetailComicsRow()],
         ],
       ),
     );
